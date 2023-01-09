961d886e055fb33540adaf297da48af1c1554458
knechtel <maiquelknechtel@gmail.com>
2023-01-09 13:36:39 -0300

    Registrando models.

--- os_backend/admin.py
+++ os_backend/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from . import models
+# Register your models here.
+from .models import Equipment
+
+
+class EquipmentInstanceInline(admin.TabularInline):
+    model = Equipment
+
+
+# inlines = [NotesInstanceInline]
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('marca',)
+
+
+admin.site.register(models.Equipment, EquipmentAdmin)
+admin.site.register(models.Client, ClientAdmin)
+
 
 # Register your models here.
